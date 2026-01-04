e1fbcd0d03b35356175ca79ca4cdb752f535ea58
AYUSHI KUMARI <162792850+ayushi48@users.noreply.github.com>
2026-01-04 21:16:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,11 +21,20 @@
 // })
 
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
 
 
